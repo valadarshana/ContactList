1d5f280886df43b0a447277e9df42612a84b7124
Vijay Parmar <vspxcoder@gmail.com>
2020-10-01 09:52:21 +0530

    Updated Code

--- ContactList/ContactList/Contact.swift
+++ ContactList/ContactList/Contact.swift
@@ -1,23 +1,25 @@
 //
-//  ContactTableViewCell.swift
+//  Contact.swift
 //  ContactList
 //
-//  Created by Vijay Parmar on 30/09/20.
+//  Created by Vijay Parmar on 01/10/20.
 //  Copyright © 2020 Dashu. All rights reserved.
 //
 
 import UIKit
 
-class ContactTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var imgProfilePic: UIImageView!
-    @IBOutlet weak var lblNumber: UILabel!
+class Contact: UITableViewCell {
+    
+    
+    @IBOutlet weak var imgProfile: UIImageView!
     @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblNumber: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        imgProfilePic.layer.cornerRadius = 40
-        imgProfilePic.layer.borderColor = UIColor.blue.cgColor
-        imgProfilePic.layer.borderWidth = 1
+        // Initialization code
+        imgProfile.layer.cornerRadius=40
+        imgProfile.layer.borderWidth=1
+        imgProfile.layer.borderColor=UIColor.blue.cgColor
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
